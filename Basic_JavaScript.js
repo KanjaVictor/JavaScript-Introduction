693c807d6c86ad9fc672d514010053e876dece42
Kanja Victor <victorkaguri77@gmail.com>
2020-11-05 23:49:14 +0300

    Initial Commit

---
+++ Basic_JavaScript.js
@@ -0,0 +1,82 @@
+//LESSON 1
+//There are two ways to write comments:
+
+//(1).In-line comment
+
+/*(2).Multi-line
+comment*/
+
+//LESSON 2
+//Declaring JavaScript Variables
+//Variables can store different values at different times.
+//Variables can be made up of numbers, letters and $ or _ but may not start with a number.
+
+//Data types
+
+/*
+  Undefined
+  Null
+  Boolean
+  String
+  Symbol
+  Bigint
+  Number
+  Float
+  Object
+
+*/
+
+//LESSON 3
+
+//(1).Storing values with the assignment operator.
+//Assignment Operator(=)
+var exampleVar;
+exampleVar = 75;
+
+//(2).Initializing Variables with the assignment operator
+var myVar = "Initializing Variables";
+/*
+Uninitialized variables have an initial value of undefined.
+->If you do a mathematical operation on an undefined variable your results will be NaN..."Not a Number"
+->If you concatenate a string with an undefined variable your result will be "undefined".
+*/
+
+//LESSON 4
+
+/*
+->Variables and Function names are case sensitive
+Best Practice: Write variableNames in camelCase.
+*/
+
+//LESSON 5
+
+//1.Arithmetic operators :integers and floats ,numeric.
+
+/*
+#Addition : +
+#Subtraction : -
+#Multiplication : *
+#Division : /
+#Remainder: % (Also known as Modulus operator)
+#Exponential : **
+
+#Increment : ++
+#Decrement : --
+*/
+
+//2.Assignment operators.
+
+/*
+
+( += )
+( -= )
+( *= )
+( /= )
+
+*/
+
+//LESSON 6
+
+//Declaring string variables
+var varName = "Your Name";
+//"Your Name" is a string literal.
