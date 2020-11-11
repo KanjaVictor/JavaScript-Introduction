a9e9f8c0906d479d93a662604a4cf7941c1ab560
Kanja Victor <victorkaguri77@gmail.com>
2020-11-11 17:19:09 +0300

    Intial Commit

---
+++ Basic_JavaScript-2.js
@@ -0,0 +1,40 @@
+//LESSON 1
+//LOOPS
+
+//IF Statements
+/*
+if(condition is true){
+  statement is executed
+}
+
+The conditions are known as Boolean conditions.
+*/
+
+//Equality Operator( == )
+/*
+The equality operator compares two values
+and returns true if they're equivalent or false if they are not.
+
+Can compare different data types: Strings and integers.
+Done through coercion.The data type is converted to one of the data types and then a comparison is made.
+*/
+
+//Strict Equality Operator( === )
+/*
+Unlike the equality counterpart, no conversion is performed.
+If different data types are used they're considered unequal.
+*/
+
+//Inequality Operator ( != )
+
+//Strict Inequality Operator( !== )
+
+//Greater than Operator( > )
+
+//Greater than or Equal to Operator( >= )
+
+//Less than Operator( < )
+
+//Less tnan of Equal to Operator( <= )
+
+//Logical And Operator( && )
